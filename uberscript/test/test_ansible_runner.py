8a0caf0cd13dde890277e9372143df1a8ef906e1
luto <m@luto.at>
2016-08-06 23:58:31 +0200

    add tests for AnsibleRunner._check_playbook

---
+++ uberscript/test/test_ansible_runner.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.parametrize("args,kwargs,valid", [
+  ([None], {}, False),
+  (['../playbook.yml'], {}, False),
+  (['/playbook.yml'], {}, True),
+])
+def test_parameter_depends(args, kwargs, valid, monkeypatch):
+  import os
+  from ..runners.ansiblerunner import AnsibleRunner
+  from ansible.errors import AnsibleFileNotFound
+
+  def alwaystrue(*args, **kwargs):
+    return True
+
+  monkeypatch.setattr(os.path, 'isfile', alwaystrue)
+
+  try:
+    if not valid:
+      with pytest.raises(ValueError):
+        AnsibleRunner(*args, **kwargs).run({}, False)
+    else:
+      AnsibleRunner(*args, **kwargs).run({}, False)
+  except AnsibleFileNotFound:
+    pass
