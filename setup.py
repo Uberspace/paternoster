e1f382398c9fc124431d5150312cda698f99fd23
luto <m@luto.at>
2020-09-03 13:39:29 +0200

    Version 3.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     sys.exit(1)
 
 setup(name='paternoster',
-      version='3.2.0',
+      version='3.2.1',
       description='Paternoster provides allows to run ansible playbooks like ordinary python or bash scripts.',
       author='uberspace.de',
       author_email='hallo@uberspace.de',
